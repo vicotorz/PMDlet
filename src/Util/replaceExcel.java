1f4245d5c0e3d6b6a729a9fe739cc5829515ff17
Victorz <victorzsnail@qq.com>
2018-04-07 20:32:36 +0800

    删除冗余注释

--- src/Util/replaceExcel.java
+++ src/Util/replaceExcel.java
@@ -15,7 +15,6 @@ public class replaceExcel {
 	ArrayList<String[]> list = new ArrayList<String[]>();
 
 	public void ReadFile(String path) {
-		// System.out.println(path);
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(path));// 换成你的文件名
 			reader.readLine();// 第一行信息，为标题信息，不用,如果需要，注释掉
@@ -27,22 +26,15 @@ public class replaceExcel {
 				// }
 				String item[] = line.split(",");// CSV格式文件为逗号分隔符文件，这里根据逗号切分
 				String repaceString = item[2];
-				// int value = Integer.parseInt(last);//如果是数值，可以转化为数值
 				String[] s = repaceString.split("\\\\");
-				// System.out.println(repaceString);
-				// System.out.println(s[0]);
 				StringBuffer finalString = new StringBuffer("\"");
 				for (int i = 4; i < s.length; i++) {
-					// System.out.println(s[i]);
 					finalString.append(s[i]);
 					finalString.append("\\");
 				}
-				// System.out.println(finalString);
 				item[2] = finalString.toString().substring(0, finalString.length() - 1);
 				// System.out.println(item[2]);
 				list.add(item);
-				// Scanner sc = new Scanner(System.in);
-				// sc.nextLine();
 			}
 			System.out.println(list.size());
 			reader.close();
@@ -71,22 +63,9 @@ public class replaceExcel {
 			while (it.hasNext()) {
 				String[] item = (String[]) it.next();
 				for (int i = 0; i < item.length; i++) {
-					// System.out.println(item[i].length());
-					// String temp;
-					// if (item[i].length() >= 3) {
-					// temp = item[i].substring(1, item[i].length() - 2);
-					// } else {
-					// temp = item[i];
-					// }
 					content[step][i] = item[i];
 				}
 				step++;
-				// System.out.println("写入句子");
-				// for (int i = 0; i < content.length; i++) {
-				// System.out.println(content[i]);
-				// }
-				// Scanner sc = new Scanner(System.in);
-				// sc.nextLine();
 			}
 			for (int i = 0; i < step; i++) {
 				writer.writeRecord(content[i]);
